f0f84b5ded20f5b11919661d47b912d928b92ba8
Administrator <Administrator@6VONETUM41T5TPY>
2020-02-18 04:01:50 +0530

    take snap

--- hellojava/src/main/java/firstmavenproject/hellojava/App.java
+++ hellojava/src/main/java/firstmavenproject/hellojava/App.java
@@ -1,9 +1,20 @@
 package firstmavenproject.hellojava;
 
-import javax.sql.rowset.WebRowSet;
 
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.ScreenshotException;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -13,9 +24,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     { 	WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
+        TakesScreenshot shot = (TakesScreenshot)driver;
+        File srcFile = shot.getScreenshotAs(OutputType.FILE);
+        File destFile = new File("ganesh.jpg");
+        FileUtils.copyFile(srcFile, destFile);
+        
     }
 }
